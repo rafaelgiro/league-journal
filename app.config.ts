831a80571e77613f010d566f25ee95c01ef12416
Rafael Giro <me@rafaelgiro.dev>
2022-04-05 09:49:59 -0300

    feat: release 1.0.0 na playstore

--- app.config.ts
+++ app.config.ts
@@ -3,5 +3,9 @@ export default {
   version: '1.0.0',
   extra: {
     apiUrl: 'https://league-journal.vercel.app/api'
+  },
+  android: {
+    package: 'dev.rafaelgiro.leaguejournal',
+    versionCode: 1
   }
 };
